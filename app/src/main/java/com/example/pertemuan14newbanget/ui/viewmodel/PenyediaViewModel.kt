91ea4827250e1038303b065cda6ad836da769d91
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-12 03:05:28 +0700

    menambahkan penyediaviewmodel

---
+++ app/src/main/java/com/example/pertemuan14newbanget/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.pertemuan14newbanget.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan14newbanget.MahasiswaApplications
+
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
