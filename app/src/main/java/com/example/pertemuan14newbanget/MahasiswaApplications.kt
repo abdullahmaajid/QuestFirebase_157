207f4bf9814d60dea199df5dda675c552fe8d405
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-12 03:08:11 +0700

    menambahkan mahassiswaapplications

---
+++ app/src/main/java/com/example/pertemuan14newbanget/MahasiswaApplications.kt
@@ -0,0 +1,13 @@
+package com.example.pertemuan14newbanget
+
+import android.app.Application
+import com.example.pertemuan14newbanget.di.AppContainer
+import com.example.pertemuan14newbanget.di.MahasiswaContainer
+
+class MahasiswaApplications: Application() {
+    lateinit var container: AppContainer
+    override fun onCreate(){
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+}
