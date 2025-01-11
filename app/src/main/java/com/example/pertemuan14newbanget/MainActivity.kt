0c805991bfa6bc7d8c3d30f2d64cde37deccf16e
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-12 03:17:07 +0700

    mengupdate main act

---
+++ app/src/main/java/com/example/pertemuan14newbanget/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.pertemuan14newbanget
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pertemuan14newbanget.ui.theme.Pertemuan14newbangetTheme
+import com.example.pertemuan14newbanget.ui.view.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pertemuan14newbangetTheme{
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(
+                        navigateToItemEntry = {}, modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
