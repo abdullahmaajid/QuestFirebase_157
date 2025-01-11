dcf14e27a6ab288ba6fef304c6fbfc2a8573eec8
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-12 02:58:56 +0700

    menambahkan repository mahasiswa

---
+++ app/src/main/java/com/example/pertemuan14newbanget/repository/MahasiswaRepository.kt
@@ -0,0 +1,12 @@
+package com.example.pertemuan14newbanget.repository
+
+import com.example.pertemuan14newbanget.model.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+interface MahasiswaRepository {
+    suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
+    suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa>
+}
