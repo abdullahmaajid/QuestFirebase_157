7d9b6c47a04d732bf6880e65d27411a1975e628b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-12 10:10:45 +0700

    mengupdate mahasiswa repository

--- app/src/main/java/com/example/pertemuan14newbanget/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/pertemuan14newbanget/repository/NetworkMahasiswaRepository.kt
@@ -1,33 +1,47 @@
 package com.example.pertemuan14newbanget.repository
 
+
+
+import android.net.wifi.aware.AwareResources
 import com.example.pertemuan14newbanget.model.Mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
+
 
 class NetworkMahasiswaRepository(
     private val firestore: FirebaseFirestore
-): MahasiswaRepository{
-    override suspend fun getMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow {
+): MahasiswaRepository {
+    override suspend fun getMahasiswa(): Flow<List<Mahasiswa>>  = callbackFlow{
         val mhsCollection = firestore.collection("Mahasiswa")
-            .orderBy("nim", Query.Direction.DESCENDING)
-            .addSnapshotListener{ value, error ->
+            .orderBy("nim", Query.Direction.ASCENDING)
+            .addSnapshotListener() { value, error ->
                 if (value != null) {
                     val mhsList = value.documents.mapNotNull {
-                        it.toObject(Mahasiswa::class.java)!!
+                        it.toObject(Mahasiswa::class.java)
                     }
                     trySend(mhsList)
                 }
+
             }
         awaitClose {
             mhsCollection.remove()
         }
+
     }
 
     override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa)
+                .await() //Membuat Method untuk  Insert Data kedalam Collection Mahasiswa
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data mahasiswa : ${e.message}") //Tambahkan Debugging untuk excpetion error
+        }
+
     }
 
     override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
@@ -38,7 +52,7 @@ class NetworkMahasiswaRepository(
         TODO("Not yet implemented")
     }
 
-    override suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa> {
+    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> {
         TODO("Not yet implemented")
     }
 
