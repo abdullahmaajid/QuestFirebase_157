aa70874e28a8681fe142c316decf2bdee866868b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-12 10:09:09 +0700

    mengupdate mahasiswa container

--- app/src/main/java/com/example/pertemuan14newbanget/di/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan14newbanget/di/MahasiswaContainer.kt
@@ -5,11 +5,10 @@ import com.example.pertemuan14newbanget.repository.NetworkMahasiswaRepository
 import com.google.firebase.firestore.FirebaseFirestore
 
 interface AppContainer {
-    val mahasiswaRepository: MahasiswaRepository
+    val mahasiswaRepository : MahasiswaRepository
 }
-
-class MahasiswaContainer: AppContainer{
-    private val firebase: FirebaseFirestore = FirebaseFirestore.getInstance()      // Sama dengan Base URL
+class MahasiswaContainer : AppContainer {
+    private val firebase: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override val mahasiswaRepository: MahasiswaRepository by lazy {
         NetworkMahasiswaRepository(firebase)
